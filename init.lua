5944d8a98ecda61d9d67a7eb926233c78814a2d5
yobome <work.ymo@outlook.com>
2024-05-20 17:21:54 +0800

    update clangd config, avoid utf encode warning

--- init.lua
+++ init.lua
@@ -447,7 +447,11 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        clangd = {},
+        clangd = {
+          capabilities = {
+            offsetEncoding = { 'utf-16' },
+          },
+        },
         -- gopls = {},
         -- pyright = {},
         rust_analyzer = {},
